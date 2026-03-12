8de79c014f16147022456f20a2ce628f5f338454
komi <clientmabanquesio2@gmail.com>
2026-03-12 17:13:00 +0100

    outils pour le depoyement

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Motdepasse123@localhost:5433/motors"  # adapte ton mot de passe
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./test.db"
+) # 
 
 
 engine = create_engine(DATABASE_URL)
