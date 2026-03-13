5bf8463b4a4016c9eaf211e4c9ce06621651a0aa
komi <clientmabanquesio2@gmail.com>
2026-03-13 19:51:59 +0100

    ajout de start.sh à render

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 # start.sh - script de démarrage pour le backend FastAPI
 
 # Active l'environnement virtuel (modifie le chemin si nécessaire)
-source ./venv/bin/activate
+#source ./venv/bin/activate
 
 # Installe les dépendances (optionnel, utile si tu veux que ça s'assure à chaque démarrage)
 #pip install -r requirements.txt
