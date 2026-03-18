0bbba3caccc48399be54bd0ec5ea6772fa8f5f7c
komi <clientmabanquesio2@gmail.com>
2026-03-18 22:07:53 +0100

    suppression du login-swagger

--- backend/auth.py
+++ backend/auth.py
@@ -26,7 +26,7 @@ def create_access_token(data: dict):
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login-swagger")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, Body
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from datetime import datetime, timedelta, timezone
-from typing import Optional
+
 
 import schemas 
 import crud 
 from database import get_db
-from auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES, verify_password
+from auth import create_access_token
 
 router = APIRouter(
     prefix="/auth",
@@ -49,19 +48,3 @@ def login(data: dict = Body(...), db: Session = Depends(get_db)):
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-# LOGIN Swagger (OAuth2PasswordRequestForm)
-# --------------------------------------------
-@router.post("/login-swagger", response_model=schemas.Token)
-def login_swagger(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    """
-    Login compatible Swagger avec form-data
-    """
-    user = crud.authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=401, detail="Email ou mot de passe incorrect")
-
-    access_token = create_access_token(
-        data={"sub": user.email, "role": user.role}
-    )
-
-    return {"access_token": access_token, "token_type": "bearer"}
